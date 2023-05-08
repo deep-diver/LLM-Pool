4bb159a029effa7f114a0d2d52ffb0426ae9bdcf
Chansung Park <deep.diver.csp@gmail.com>
2023-05-08 14:35:14 +0000

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='llmpool',
-    version='0.1.1',
+    version='0.1.2',
     description="Large Language Models' pool management library",
     long_description=long_description,
     long_description_content_type='text/markdown',
--- src/llmpool/__init__.py
+++ src/llmpool/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from .local_model import LocalLLModel, LocalLoRALLModel
 from .model_pool import LLModelPool
