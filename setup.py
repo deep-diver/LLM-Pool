beddae131b9595bf3df4f47cc77543a1944ef784
Chansung Park <deep.diver.csp@gmail.com>
2023-05-09 03:01:29 +0000

    version up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='llmpool',
-    version='0.1.3',
+    version='0.1.4',
     description="Large Language Models' pool management library",
     long_description=long_description,
     long_description_content_type='text/markdown',
